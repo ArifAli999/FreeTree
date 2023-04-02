788465b8ce5687f109063b129db68e3c9fa72bd4
Riffyraff <82163631+ArifAli999@users.noreply.github.com>
2023-04-02 14:23:49 +0100

    iconbuttons

--- components/Helpers/AccountSetup.tsx
+++ components/Helpers/AccountSetup.tsx
@@ -1,53 +1,54 @@
 import { Label } from '@radix-ui/react-dropdown-menu'
 import { AppleIcon, FacebookIcon, InstagramIcon, LinkedinIcon, TwitterIcon, YoutubeIcon } from 'lucide-react'
-import React from 'react'
-import {
-    AlertDialog,
-    AlertDialogAction,
-    AlertDialogCancel,
-    AlertDialogContent,
-    AlertDialogDescription,
-    AlertDialogFooter,
-    AlertDialogHeader,
-    AlertDialogTitle,
-    AlertDialogTrigger,
-} from "@/components/ui/alert-dialog"
+import React, { useState } from 'react';
 import { Button } from "@/components/ui/button"
 
 import { SheetHeader, SheetTitle, SheetDescription } from '../ui/sheet'
+import { Input } from '../ui/input'
+import SocialMediaIcon from './SocialMediaIcon'
 
 
-function AccountDialog() {
 
-    return (
-        <AlertDialog defaultOpen={true} open={true}>
+function AccountSetup() {
 
-            <AlertDialogContent>
-                <AlertDialogHeader>
-                    <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-                    <AlertDialogDescription>
-                        This action cannot be undone. This will permanently delete your
-                        account and remove your data from our servers.
-                    </AlertDialogDescription>
-                </AlertDialogHeader>
-                <AlertDialogFooter>
-                    <AlertDialogCancel>Cancel</AlertDialogCancel>
-                    <AlertDialogAction>Continue</AlertDialogAction>
-                </AlertDialogFooter>
-            </AlertDialogContent>
-        </AlertDialog>
-    )
-}
-function AccountSetup({ accounts, setAccounts }) {
+    const [facebookLink, setFacebookLink] = useState('');
+    const [twitterLink, setTwitterLink] = useState('');
+    const [instagramLink, setInstagramLink] = useState('');
+    const [links, setLinks] = useState({
+        facebook: '',
+        instagram: '',
+        twitter: '',
+    })
 
+    function handleFacebookLinkChange(value) {
+        setFacebookLink(value);
+        setLinks({
+            facebook: value,
+            instagram: instagramLink,
+            twitter: twitterLink
+        })
+    }
 
-    function openDialog(name: string) {
-        console.log('test')
-        return (
-            <AccountDialog />
-        )
+    function handleTwitterLinkChange(value) {
+        setTwitterLink(value);
+        setLinks({
+            facebook: facebookLink,
+            instagram: instagramLink,
+            twitter: value,
+        })
 
     }
+
+    function handleInstagramLinkChange(value) {
+
+        setInstagramLink(value);
+        setLinks({
+            facebook: facebookLink,
+            instagram: value,
+            twitter: twitterLink
+        })
+    }
+
     return (
         <><SheetHeader>
             <SheetTitle>ACCOUNT SETUP</SheetTitle>
@@ -56,40 +57,22 @@ function AccountSetup({ accounts, setAccounts }) {
                 to see some magic!
             </SheetDescription>
         </SheetHeader>
-            <div className="flex flex-col gap-4 py-4 mt-0">
-                <div className="grid w-full  items-center gap-1 mb-2">
-                    <Label className='text-xs dark:text-slate-100 mb-1 leading-relaxed'>Pick your accounts</Label>
+            <div className="flex flex-col gap-4 py-4 mt-6">
+                <div className="flex w-full  items-center gap-1 mb-2">
+                    <Label className='text-sm dark:text-slate-100 mb-1 leading-relaxed'>Pick your accounts</Label>
 
                 </div>
 
-                <div className='flex flex-row items-center gap-10'>
-                    <div className='bg-slate-600 p-3 cursor-pointer  rounded-md' onClick={() => openDialog('Facebook')}>
-
-                        <FacebookIcon />
-                    </div>
-                    <div className='bg-slate-600 p-3 cursor-pointer rounded-md'>
-                        <TwitterIcon />
-                    </div>
-                    <div className='bg-slate-600 p-3 cursor-pointer rounded-md'>
-                        <InstagramIcon />
-                    </div>  <div className='bg-slate-600 p-3 cursor-pointer rounded-md'>
-                        <LinkedinIcon />
-                    </div>
-                    <div className='bg-slate-600 p-3 cursor-pointer rounded-md'>
-                        <AppleIcon />
-                    </div>
-                    <div className='bg-slate-600 p-3 cursor-pointer rounded-md'>
-                        <YoutubeIcon />
-                    </div>
+                <div className='flex flex-row items-center gap-8'>
+                    <SocialMediaIcon name='Facebook' onChange={handleFacebookLinkChange} />
+                    <SocialMediaIcon name='Twitter' onChange={handleTwitterLinkChange} />
+                    <SocialMediaIcon name='Instagram' onChange={handleInstagramLinkChange} />
                 </div>
 
-                <div className="flex flex-col w-full  gap-2.5 relative min-h-[320px]">
-                    <div className='flex flex-row items-center justify-between mb-25 '>
 
 
-                    </div>
 
-                </div>
+
             </div></>
     )
 }
